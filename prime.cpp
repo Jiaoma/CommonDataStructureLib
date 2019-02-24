0928a6867db7bc7d94b2447eb2b1207f2bbb34dd
Jiaoma <3258594@163.com>
2019-02-24 14:36:34 +0800

    ok

--- prime.cpp
+++ prime.cpp
@@ -10,6 +10,8 @@ int isPrime(int n)
         return 0;
     if(n==2)
         return 1;
+    if(n%2==0)
+    	return 0;
     for(i=3;i<=sqrt(n);i+=2)
         if(n%i==0)
             return 0;
@@ -19,7 +21,7 @@ int isPrime(int n)
 
 //筛法求素数
 //sieve of Eratosthenes
-void EraPrime(int *storage,int n)
+void EraPrime(int *&storage,int n)
 {
     int cur;
     storage=new int(n+5);
@@ -39,21 +41,29 @@ void EraPrime(int *storage,int n)
                 storage[j*cur]=0;
         }
     }
-    for(int k=0;k<=n;k++)
-        if(storage[k])
-            cout<<k<<endl;
 }
 void printArray(int *s,int len)
 {
-    for(int i=0;i<len-5;i++)
+    for(int i=0;i<len;i++)
     {
-        cout<<*(s+i)<<endl;
+        if(s[i])
+             cout<<i<<endl;
     }
 }
 int main()
 {
     int *nums;
     EraPrime(nums,100);
-    //printArray(nums,100);
+    printArray(nums,100);
+    cout<<isPrime(4)<<endl;
+    for(int i=0;i<100;i++)
+    	if((nums[i]==1 && not isPrime(i)) || (nums[i]==0 && isPrime(i)))
+    	{
+    		cout<<"Error"<<endl;
+    		cout<<i<<endl;
+    		return 0;
+    	}
+    cout<<"OK"<<endl;
+
     //cout<<*nums<<endl;
 }
